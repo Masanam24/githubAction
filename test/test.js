0d820eb1d21f6bead789d31d883d9d2f1ce24aa0
Choirul Anam <choirul.anam@auk.industries>
2022-04-12 16:44:50 +0700

    test simple CRUD

--- test/test.js
+++ test/test.js
@@ -1,8 +1,33 @@
 const request = require("supertest");
-const app = require("../index");
+const server = require("../index");
 
-describe("GET /", () => {
-  it("respond with Hello World", (done) => {
-    request(app).get("/").expect("Hello World", done);
-  })
-});
+//Require the dev-dependencies
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+
+
+chai.use(chaiHttp);
+//Our parent block
+// describe('Books', () => {
+//     beforeEach((done) => { //Before each test we empty the database
+//         Book.remove({}, (err) => { 
+//            done();           
+//         });        
+//     });
+/*
+  * Test the /GET route
+  */
+  describe('/GET data', () => {
+      it('it should GET all the datas', (done) => {
+        chai.request(server)
+            .get('/tutorial')
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('array');
+                  res.body.length.should.be.eql(0);
+              done();
+            });
+      });
+  });
+
